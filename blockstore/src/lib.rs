71d6d83fdfe59431118bd5b618b725dfe2d4bb45
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2022-02-08 14:19:01 +0000

    feat: evict method

--- blockstore/src/lib.rs
+++ blockstore/src/lib.rs
@@ -16,6 +16,12 @@ impl<T: types::Store> DbBlockStore<T> {
     fn get_bytes(&self, cid: &Cid) -> Result<Option<Vec<u8>>, Box<dyn StdError>> {
         Ok(self.db.read(cid.to_bytes())?)
     }
+    fn delete_bytes(&self, cid: &Cid) -> Result<(), Box<dyn StdError>> {
+        Ok(self.db.delete(cid.to_bytes())?)
+    }
+    fn bytes_exist(&self, cid: &Cid) -> Result<bool, Box<dyn StdError>> {
+        Ok(self.db.exists(cid.to_bytes())?)
+    }
 }
 /// Wrapper for database to handle inserting and retrieving ipld data with Cids
 pub trait BlockStore {
@@ -24,6 +30,10 @@ pub trait BlockStore {
     fn get(&self, cid: &Cid) -> Result<Block<Self::Params>, Box<dyn StdError>>;
 
     fn insert(&self, block: &Block<Self::Params>) -> Result<(), Box<dyn StdError>>;
+
+    fn evict(&self, cid: &Cid) -> Result<(), Box<dyn StdError>>;
+
+    fn contains(&self, cid: &Cid) -> Result<bool, Box<dyn StdError>>;
 }
 
 impl<T: types::Store> BlockStore for DbBlockStore<T> {
@@ -41,6 +51,14 @@ impl<T: types::Store> BlockStore for DbBlockStore<T> {
         let bytes = block.data();
         let cid = &block.cid().to_bytes();
         Ok(self.db.write(cid, bytes)?)
+    }
+
+    fn evict(&self, cid: &Cid) -> Result<(), Box<dyn StdError>> {
+        self.delete_bytes(cid)
+    }
+
+    fn contains(&self, cid: &Cid) -> Result<bool, Box<dyn StdError>> {
+        self.bytes_exist(cid)
     }
 }
 
@@ -73,8 +91,31 @@ mod tests {
     }
 
     #[test]
+    fn test_mem_delete_block() {
+        let mem_db = db::MemoryDB::default();
+        let store = DbBlockStore { db: mem_db };
+
+        let leaf1 = ipld!({ "name": "leaf1", "size": 12 });
+        let leaf1_block = Block::encode(DagCborCodec, Code::Sha2_256, &leaf1).unwrap();
+        store.insert(&leaf1_block.clone()).unwrap();
+
+        let leaf2 = ipld!({ "name": "leaf2", "size": 6 });
+        let leaf2_block = Block::encode(DagCborCodec, Code::Sha2_256, &leaf2).unwrap();
+        store.insert(&leaf2_block.clone()).unwrap();
+
+        store.evict(leaf1_block.cid()).unwrap();
+        store.evict(leaf2_block.cid()).unwrap();
+
+        let exists_leaf1 = store.contains(leaf1_block.cid()).unwrap();
+        let exists_leaf2 = store.contains(leaf2_block.cid()).unwrap();
+
+        assert_eq!(false, exists_leaf1);
+        assert_eq!(false, exists_leaf2);
+    }
+
+    #[test]
     fn test_db_recovers_block() {
-        let path = DBPath::new("write_test");
+        let path = DBPath::new("get_test");
         let db = db::Db::open(path.as_ref()).unwrap();
         let store = DbBlockStore { db: db };
 
@@ -91,5 +132,29 @@ mod tests {
 
         assert_eq!(leaf1_block, leaf1_recovered_block);
         assert_eq!(leaf2_block, leaf2_recovered_block);
+    }
+
+    #[test]
+    fn test_db_delete_block() {
+        let path = DBPath::new("evict_test");
+        let db = db::Db::open(path.as_ref()).unwrap();
+        let store = DbBlockStore { db: db };
+
+        let leaf1 = ipld!({ "name": "leaf1", "size": 12 });
+        let leaf1_block = Block::encode(DagCborCodec, Code::Sha2_256, &leaf1).unwrap();
+        store.insert(&leaf1_block.clone()).unwrap();
+
+        let leaf2 = ipld!({ "name": "leaf2", "size": 6 });
+        let leaf2_block = Block::encode(DagCborCodec, Code::Sha2_256, &leaf2).unwrap();
+        store.insert(&leaf2_block.clone()).unwrap();
+
+        store.evict(leaf1_block.cid()).unwrap();
+        store.evict(leaf2_block.cid()).unwrap();
+
+        let exists_leaf1 = store.contains(leaf1_block.cid()).unwrap();
+        let exists_leaf2 = store.contains(leaf2_block.cid()).unwrap();
+
+        assert_eq!(false, exists_leaf1);
+        assert_eq!(false, exists_leaf2);
     }
 }

0faeca85b7576971ccbe9b146b3dceaac7f99233
tchardin <tdotchardin@gmail.com>
2022-02-11 10:35:44 -0600

    feat: handle missing blocks on the provider side

--- graphsync/src/response_manager.rs
+++ graphsync/src/response_manager.rs
@@ -46,6 +46,7 @@ pub struct ResponseBuilder<P: StoreParams> {
     req_id: RequestId,
     peer_id: PeerId,
     size: usize,
+    partial: bool,
     blocks: VecDeque<Block<P>>,
     sender: Arc<Sender<ResponseEvent>>,
     sent: HashSet<Cid>,
@@ -59,6 +60,7 @@ impl<P: StoreParams> ResponseBuilder<P> {
             req_id,
             peer_id,
             size: 0,
+            partial: false,
             blocks: VecDeque::new(),
             sender,
             sent: HashSet::new(),
@@ -72,7 +74,7 @@ impl<P: StoreParams> ResponseBuilder<P> {
         // batch all blocks together until we get to the max size
         if next_size > MAX_BLOCK_SIZE {
             // flush the blocks to the network
-            self.send(ResponseStatusCode::RequestCompletedPartial);
+            self.send(ResponseStatusCode::PartialResponse);
         }
         self.metadata.push(MetadataItem {
             link: CidCbor::from(block.cid().clone()),
@@ -81,8 +83,22 @@ impl<P: StoreParams> ResponseBuilder<P> {
         self.blocks.push_back(block);
         self.size = self.size + block_size;
     }
+    pub fn missing_block(&mut self, cid: &Cid) {
+        self.metadata.push(MetadataItem {
+            link: CidCbor::from(cid.clone()),
+            block_present: false,
+        });
+        self.partial = true;
+    }
     pub fn completed(&mut self) {
-        self.send(ResponseStatusCode::RequestCompletedFull);
+        let event = {
+            if self.partial {
+                ResponseStatusCode::RequestCompletedPartial
+            } else {
+                ResponseStatusCode::RequestCompletedFull
+            }
+        };
+        self.send(event);
     }
     pub fn failed(&mut self, _err: String) {
         self.send(ResponseStatusCode::RequestFailedUnknown);
@@ -115,6 +131,9 @@ impl<P: StoreParams> ResponseBuilder<P> {
         }
         let event = match status {
             ResponseStatusCode::RequestCompletedFull => ResponseEvent::Completed(self.peer_id, msg),
+            ResponseStatusCode::RequestCompletedPartial => {
+                ResponseEvent::Completed(self.peer_id, msg)
+            }
             _ => ResponseEvent::Partial(self.peer_id, msg),
         };
         match self.sender.try_send(event) {
@@ -164,7 +183,7 @@ where
         let id = request.id;
         let mut builder = ResponseBuilder::new(id, peer, sender.clone());
         // validate request and add any extensions
-        let (approved, extensions) = (hooks.read().unwrap().incoming_request)(peer, request);
+        let (approved, extensions) = (hooks.read().unwrap().incoming_request)(&peer, request);
         builder.add_extensions(extensions);
         if !approved {
             builder.rejected();
@@ -179,23 +198,25 @@ where
         spawn(async move {
             if let Ok(block) = store.get(&root) {
                 builder.add_extensions((hooks.read().unwrap().outgoing_block)(
-                    peer,
-                    root,
+                    &peer,
+                    &root,
                     block.data().len(),
                 ));
                 builder.add_block(block.clone());
                 if let Ok(node) = block.ipld() {
-                    let loader = BlockCallbackLoader::new(store, |block| {
+                    let loader = BlockCallbackLoader::new(store, |link, block| {
                         match block {
                             Some(block) => {
                                 builder.add_extensions((hooks.read().unwrap().outgoing_block)(
-                                    peer,
-                                    block.cid().clone(),
+                                    &peer,
+                                    block.cid(),
                                     block.data().len(),
                                 ));
                                 builder.add_block(block);
                             }
-                            None => {}
+                            None => {
+                                builder.missing_block(link);
+                            }
                         };
                         Ok(())
                     });
@@ -231,6 +252,7 @@ mod tests {
     use libipld::cbor::DagCborCodec;
     use libipld::ipld;
     use libipld::multihash::Code;
+    use libipld::store::DefaultParams;
 
     struct TestData {
         root: Cid,
@@ -303,6 +325,81 @@ mod tests {
                 msg.responses.get(&1).unwrap().status,
                 ResponseStatusCode::RequestCompletedFull
             );
+        } else {
+            panic!("received unexpected event");
+        };
+    }
+
+    #[async_std::test]
+    async fn response_missing_block() {
+        use serde_cbor::from_slice;
+
+        let store = Arc::new(MemoryBlockStore::default());
+
+        let leaf1 = ipld!({ "name": "leaf1", "size": 12 });
+        let leaf1_block = Block::encode(DagCborCodec, Code::Sha2_256, &leaf1).unwrap();
+        store.insert(&leaf1_block).unwrap();
+
+        // leaf2 is missing in the blockstore
+        let leaf2 = ipld!({ "name": "leaf2", "size": 6 });
+        let leaf2_block =
+            Block::<DefaultParams>::encode(DagCborCodec, Code::Sha2_256, &leaf2).unwrap();
+
+        let parent = ipld!({
+            "children": [leaf1_block.cid(), leaf2_block.cid()],
+            "favouriteChild": leaf2_block.cid(),
+            "name": "parent",
+        });
+        let parent_block = Block::encode(DagCborCodec, Code::Sha2_256, &parent).unwrap();
+        store.insert(&parent_block).unwrap();
+
+        let hooks = Arc::new(RwLock::new(GraphsyncHooks::default()));
+
+        let manager = ResponseManager::new(store, hooks);
+
+        let selector = Selector::ExploreRecursive {
+            limit: RecursionLimit::None,
+            sequence: Box::new(Selector::ExploreAll {
+                next: Box::new(Selector::ExploreRecursiveEdge),
+            }),
+            current: None,
+        };
+
+        manager.inject_request(
+            PeerId::random(),
+            &GraphsyncRequest {
+                root: parent_block.cid().clone(),
+                selector,
+                id: 1,
+                extensions: Default::default(),
+            },
+        );
+        if let Ok(ResponseEvent::Accepted(_peer, req)) =
+            manager.receiver.lock().unwrap().recv().await
+        {
+            assert_eq!(req.id, 1);
+        } else {
+            panic!("received unexpected event");
+        }
+        if let Ok(ResponseEvent::Completed(_peer, msg)) =
+            manager.receiver.lock().unwrap().recv().await
+        {
+            assert_eq!(msg.responses.len(), 1);
+            assert_eq!(msg.blocks.len(), 2);
+            assert_eq!(
+                msg.responses.get(&1).unwrap().status,
+                ResponseStatusCode::RequestCompletedPartial
+            );
+
+            let metabytes = msg.responses[&1]
+                .extensions
+                .get(METADATA_EXTENSION)
+                .unwrap();
+            let metadata: Vec<MetadataItem> = from_slice(metabytes).unwrap();
+            metadata
+                .iter()
+                .find(|item| item.block_present == false)
+                .unwrap();
         } else {
             panic!("received unexpected event");
         };

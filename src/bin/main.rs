968923987a5daf1101200fdd836b1e0e3631ce31
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2022-02-11 10:27:23 +0000

    feat: unbounded lfu as default for native build

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use blockstore::db::Db as BlockstoreDB;
+use blockstore::lfu::LfuBlockstore;
 use libipld::Cid;
 use libp2p::{Multiaddr, PeerId};
 use pop::{Node, NodeConfig};
@@ -10,7 +11,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = NodeConfig {
         listening_multiaddr: "/ip4/0.0.0.0/tcp/0/ws".parse()?,
         wasm_external_transport: None,
-        blockstore: BlockstoreDB::open("path")?,
+        blockstore: LfuBlockstore::new(0, BlockstoreDB::open("path")?)?,
     };
     let mut node = Node::new(config);
 

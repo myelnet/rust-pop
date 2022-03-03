52c558854de97af6b10feaf4508fbe6148c9e18d
tchardin <tdotchardin@gmail.com>
2022-03-03 13:39:50 -0600

    feat: parallel requests from same peer identity

--- src/browser/mod.rs
+++ src/browser/mod.rs
@@ -16,7 +16,7 @@ use libp2p::{
     core::muxing::StreamMuxerBox,
     core::transport::{Boxed, OptionalTransport, Transport},
     identity, mplex, noise,
-    swarm::{SwarmBuilder, SwarmEvent},
+    swarm::{Swarm, SwarmBuilder, SwarmEvent},
     Multiaddr, PeerId,
 };
 use routing::{Config as PeerDiscoveryConfig, PeerDiscovery};
@@ -43,7 +43,6 @@ pub use console_log::init_with_level as init_console_log;
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RequestParams {
-    pub log_level: String,
     pub maddress: String,
     pub peer_id: String,
     pub cid: String,
@@ -55,155 +54,129 @@ pub struct ResponseHeaders {
     pub content_type: String,
 }
 
-/// request initialize a transfer from the main thread. It spins up a dedicated worker with shared
-/// memory and receive each block via the on_message callback. The blocks are then streamed through
-/// an mpsc chanel which is translated into a JS readable stream sent via a promise that resolve to
-/// a Response object akin to the watwg Fetch api.
 #[wasm_bindgen]
-pub fn request(js_params: JsValue) -> Result<Promise, JsValue> {
-    let params: RequestParams = js_params.into_serde().map_err(js_err)?;
-
-    console_error_panic_hook::set_once();
-    init_console_log(log::Level::from_str(&params.log_level).unwrap()).unwrap();
-
-    let wparams = JsValue::from_serde(&params).unwrap();
-
-    // initialize a new thread with shared memory for the transfer.
-    let worker_handle = Worker::new("worker-main.js").unwrap();
-    let array = js_sys::Array::new();
-    array.push(&wasm_bindgen::module());
-    array.push(&wasm_bindgen::memory());
-    worker_handle.post_message(&array)?;
-
-    // this sender receives the content type, meaning that we received the first bytes.
-    let (os, or) = oneshot::channel();
-    let mut osender = Some(os);
-    // the bytes are streamed through this channel.
-    let (mut sender, r) = mpsc::channel(64);
-    // set the worker callback, the first call will take ownership of the oneshot sender so it
-    // knows to check the first bytes for the content type. After that all bytes are sent to the
-    // mpsc returned by the promise.
-    let onmessage = Closure::wrap(Box::new(move |event: MessageEvent| {
-        if event.data().is_undefined() {
-            sender.close_channel()
-        } else if let Some(os) = osender.take() {
-            if let Ok(buf) = event.data().dyn_into::<js_sys::Uint8Array>() {
-                let ct = detect_content_type(&buf.to_vec());
-                drop(os.send(ct));
-                sender.start_send(event.data()).unwrap();
-            }
-        } else {
-            sender.start_send(event.data()).unwrap();
-        }
-    }) as Box<dyn FnMut(_)>);
-    worker_handle.set_onmessage(Some(onmessage.as_ref().unchecked_ref()));
-    onmessage.forget();
-
-    // start the request
-    worker_handle.post_message(&wparams)?;
-
-    // the returned promise waits for the content type to be sent and then returns the stream to
-    // start reading chunks.
-    let done = async move {
-        match or.await {
-            Ok(ct) => {
-                // the mpsc receiver is mapped into a readable stream source.
-                let source = IntoUnderlyingSource::new(Box::new(r.map(Ok)));
-                // Set HWM to 0 to prevent the JS ReadableStream from buffering chunks in its queue,
-                // since the original Rust stream is better suited to handle that.
-                let strategy = QueuingStrategy::new(0.0);
-
-                let js_stream: JsStream =
-                    ReadableStream::new_with_source(source, strategy).unchecked_into();
-
-                let headers =
-                    JsValue::from_serde(&ResponseHeaders { content_type: ct }).map_err(js_err)?;
-
-                let mut init = ResponseInit::new();
-
-                let res = Response::new_with_opt_readable_stream_and_init(
-                    Some(&js_stream),
-                    init.headers(&headers).status(200),
-                )?;
-                Ok(res.into())
-            }
-            Err(_) => Err(JsValue::undefined()),
-        }
-    };
-    Ok(wasm_bindgen_futures::future_to_promise(done))
+pub struct Node {
+    transport: Boxed<(PeerId, StreamMuxerBox)>,
+    local_peer_id: PeerId,
 }
 
-/// bg_request does the actual request, it runs on a background thread so as not to block the main
-/// thread. For now it spins a new node each time but there should be a way to pass the swarm so
-/// a single instance of the node runs and spawns parallel requests in different workers.
+// encapsulates a common transport and peer identity for each request.
+// will throw an error if initialized twice.
 #[wasm_bindgen]
-pub async fn bg_request(js_params: JsValue) -> Result<(), JsValue> {
-    let global = js_sys::global().unchecked_into::<DedicatedWorkerGlobalScope>();
-    let params: RequestParams = js_params.into_serde().map_err(js_err)?;
+impl Node {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        console_error_panic_hook::set_once();
+        init_console_log(log::Level::from_str("info").unwrap()).unwrap();
 
-    let maddr: Multiaddr = params.maddress.parse().map_err(js_err)?;
-    let peer_id = PeerId::from_str(&params.peer_id).map_err(js_err)?;
+        let local_key = identity::Keypair::generate_ed25519();
+        let local_peer_id = PeerId::from(local_key.public());
+        log::info!("Local peer id: {:?}", local_peer_id);
 
-    let (cid, selector) =
-        unixfs_path_selector(params.cid).ok_or(JsValue::from_str("invalid cid path"))?;
-
-    let store = Arc::new(CacheStore::default());
-
-    let local_key = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(local_key.public());
-    log::info!("Local peer id: {:?}", local_peer_id);
-
-    let transport = build_transport(local_key.clone());
-
-    let behaviour = DataTransfer::new(
-        local_peer_id,
-        Graphsync::new(GraphsyncConfig::default(), store.clone()),
-        PeerDiscovery::new(PeerDiscoveryConfig::default(), local_peer_id),
-    );
-
-    let mut swarm = SwarmBuilder::new(transport, behaviour, local_peer_id)
-        .executor(Box::new(|fut| {
-            wasm_bindgen_futures::spawn_local(fut);
-        }))
-        .build();
-
-    swarm.behaviour_mut().add_address(&peer_id, maddr);
-
-    let params = DealParams {
-        selector: Some(selector.clone()),
-        ..Default::default()
-    };
-
-    swarm
-        .behaviour_mut()
-        .pull(peer_id, cid, selector, params)
-        .map_err(js_err)?;
-
-    // we loop through swarm events and send chunks as shared memory back to the main thread. To
-    // resolve the content from the IPLD DAG we check for the bytes enum as these are the actual
-    // chunks of the DAGifyed content.
-    while let Some(evt) = swarm.next().await {
-        if let SwarmEvent::Behaviour(event) = evt {
-            match event {
-                DataTransferEvent::Block {
-                    data: Ipld::Bytes(bytes),
-                    ..
-                } => {
-                    let data = js_sys::Uint8Array::from(bytes.as_slice());
-                    global.post_message_with_transfer(&data, &data.buffer())?;
-                }
-                DataTransferEvent::Completed(_, Ok(())) => {
-                    break;
-                }
-                _ => {}
-            }
+        Self {
+            transport: build_transport(local_key.clone()),
+            local_peer_id,
         }
     }
-    // once the transfer is complete we break out the loop and send an undefined value to notify
-    // main thread the work is compete.
-    global.post_message(&JsValue::undefined())?;
+    pub fn spawn_request(&self, js_params: JsValue, pool: &WorkerPool) -> Result<Promise, JsValue> {
+        let store = Arc::new(CacheStore::default());
 
-    Ok(())
+        let behaviour = DataTransfer::new(
+            self.local_peer_id,
+            Graphsync::new(GraphsyncConfig::default(), store.clone()),
+            PeerDiscovery::new(PeerDiscoveryConfig::default(), self.local_peer_id),
+        );
+        // swarm is not safe to share between threads so instead of wrapping into a mutex
+        // we create a new instance each time with the same transport backed by the same peer ID
+        let mut swarm = SwarmBuilder::new(self.transport.clone(), behaviour, self.local_peer_id)
+            .executor(Box::new(|fut| {
+                wasm_bindgen_futures::spawn_local(fut);
+            }))
+            .build();
+        // JsValue is not safe to share between thread so we must deserialize on main thread
+        let params: RequestParams = js_params.into_serde().unwrap();
+
+        // this sender receives the content type, meaning that we received the first bytes.
+        let (os, or) = oneshot::channel();
+        // the bytes are streamed through this channel.
+        let (mut sender, r) = mpsc::channel(64);
+        pool.run(move || {
+            let maddr: Multiaddr = params.maddress.parse().unwrap();
+            let peer_id = PeerId::from_str(&params.peer_id).unwrap();
+
+            let (cid, selector) = unixfs_path_selector(params.cid).unwrap();
+
+            swarm.behaviour_mut().add_address(&peer_id, maddr);
+
+            let params = DealParams {
+                selector: Some(selector.clone()),
+                ..Default::default()
+            };
+
+            swarm
+                .behaviour_mut()
+                .pull(peer_id, cid, selector, params)
+                .unwrap();
+
+            // future is executed locally inside the thread
+            wasm_bindgen_futures::spawn_local(async move {
+                let mut osender = Some(os);
+                // we loop through swarm events and send chunks as shared memory back to the main thread. To
+                // resolve the content from the IPLD DAG we check for the bytes enum as these are the actual
+                // chunks of the DAGifyed content.
+                while let Some(evt) = swarm.next().await {
+                    if let SwarmEvent::Behaviour(event) = evt {
+                        match event {
+                            DataTransferEvent::Block {
+                                data: Ipld::Bytes(bytes),
+                                ..
+                            } => {
+                                if let Some(os) = osender.take() {
+                                    let ct = detect_content_type(&bytes);
+                                    drop(os.send(ct));
+                                }
+                                sender.start_send(bytes).unwrap();
+                            }
+                            DataTransferEvent::Completed(_, Ok(())) => {
+                                break;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            });
+        })?;
+        let done = async move {
+            match or.await {
+                Ok(ct) => {
+                    // the mpsc receiver is mapped into a readable stream source.
+                    let source = IntoUnderlyingSource::new(Box::new(r.map(|bytes| {
+                        let data: JsValue = js_sys::Uint8Array::from(bytes.as_slice()).into();
+                        Ok(data)
+                    })));
+                    // Set HWM to 0 to prevent the JS ReadableStream from buffering chunks in its queue,
+                    // since the original Rust stream is better suited to handle that.
+                    let strategy = QueuingStrategy::new(0.0);
+
+                    let js_stream: JsStream =
+                        ReadableStream::new_with_source(source, strategy).unchecked_into();
+
+                    let headers = JsValue::from_serde(&ResponseHeaders { content_type: ct })
+                        .map_err(js_err)?;
+
+                    let mut init = ResponseInit::new();
+
+                    let res = Response::new_with_opt_readable_stream_and_init(
+                        Some(&js_stream),
+                        init.headers(&headers).status(200),
+                    )?;
+                    Ok(res.into())
+                }
+                Err(_) => Err(JsValue::undefined()),
+            }
+        };
+        Ok(wasm_bindgen_futures::future_to_promise(done))
+    }
 }
 
 fn js_err<E: ToString + Send + Sync + 'static>(e: E) -> JsValue {
@@ -230,63 +203,6 @@ pub fn build_transport(local_key: identity::Keypair) -> Boxed<(PeerId, StreamMux
         .multiplex(mplex_config)
         .timeout(Duration::from_secs(20))
         .boxed()
-}
-
-#[wasm_bindgen]
-pub struct DagService {
-    store: Arc<BlockstoreMemory>,
-}
-
-#[wasm_bindgen]
-impl DagService {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Self {
-            store: Arc::new(BlockstoreMemory::default()),
-        }
-    }
-    pub fn string_to_block(self, value: String, pool: &WorkerPool) -> Result<Promise, JsValue> {
-        let store = self.store;
-        let (tx, rx) = oneshot::channel();
-        pool.run(move || {
-            let node = Ipld::String(value);
-            let block = Block::encode(DagCborCodec, Code::Sha2_256, &node).unwrap();
-            store.insert(&block).unwrap();
-
-            drop(tx.send(block.cid().to_string()));
-        })?;
-
-        let done = async move {
-            match rx.await {
-                Ok(cid) => Ok(cid.into()),
-                Err(_) => Err(JsValue::undefined()),
-            }
-        };
-        Ok(wasm_bindgen_futures::future_to_promise(done))
-    }
-
-    pub fn bytes_to_blocks(self, value: Uint8Array, pool: &WorkerPool) -> Result<Promise, JsValue> {
-        let store = self.store;
-
-        let (tx, rx) = oneshot::channel();
-
-        let data = value.to_vec();
-
-        pool.run(move || {
-            if let Ok(res) = dag_service::add(store, &data) {
-                drop(tx.send(res));
-            } else {
-                drop(tx.send(None));
-            }
-        })?;
-        let done = async move {
-            match rx.await {
-                Ok(Some(cid)) => Ok(cid.to_string().into()),
-                _ => Err("Failed to add dag".to_string().into()),
-            }
-        };
-        Ok(wasm_bindgen_futures::future_to_promise(done))
-    }
 }
 
 #[wasm_bindgen]
